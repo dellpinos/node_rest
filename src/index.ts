31f29469b8f5f9aae71826785ee9987c905d6284
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2024-09-06 21:25:49 -0300

    logs colors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import server from "./server";
+import colors from 'colors';
 
+const port = process.env.PORT || 4000;
 server.listen(4000, () => {
-    console.log(`Express en ejecución en el puerto 4000`)
+    console.log( colors.bgCyan.black.bold(`Express en ejecución en el puerto ${port}`));
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,26 @@
 import express from "express";
 import router from "./routes";
+import db from "./config/db";
+import colors from 'colors';
+
+
+// Conectar DB
+async function connectDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log(colors.bgGreen.black.bold('Conexión exitosa a la DB'));
+    } catch (error) {
+        console.log( colors.bgRed.white.bold('Hubo un error al conectar la DB'));
+        console.log(error);
+    }
+}
+
+connectDB();
 
 const server = express();
 
+// Routes
 server.use('/api/products', router);
 
 export default server;
