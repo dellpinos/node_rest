ac8b1484d618d2b2becb9185dfb301b2eba4e93c
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2024-09-09 22:53:26 -0300

    definiendo rutas get

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,9 +1,26 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
+import colors from 'colors';
+
+export const getProducts = async (req : Request, res : Response) => {
+    try {
+        const products = await Product.findAll({
+            order: [
+                ['price', 'DESC']
+            ]
+        });
+        res.json({data: products});
+    } catch (error) {
+        console.log( colors.bgRed.white.bold(error) );
+    }
+}
 
 export const createProduct = async (req : Request, res : Response) => {
 
-    const product = await Product.create(req.body);
-
-    res.json({data: product});
+    try {
+        const product = await Product.create(req.body);
+        res.json({data: product});
+    } catch (error) {
+        console.log( colors.bgRed.white.bold(error) );
+    }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,25 @@
 import { Router } from 'express';
-import { createProduct } from './handlers/product';
+import { body } from 'express-validator';
+import { createProduct, getProducts } from './handlers/product';
+import { handleInputErrors } from './middleware';
 
 const router = Router();
 
 // Routing
-router.get('/', (req, res) => {
-    res.json("Desde GET");
-});
+router.get('/', getProducts);
 
-router.post('/', createProduct);
+router.post('/', 
+
+    // Validación
+    body('name').notEmpty().withMessage('El nombre del producto es obligatorio'),
+    body('price')
+        .isNumeric().withMessage('Formato inválido')
+        .notEmpty().withMessage('El precio del producto es obligatorio')
+        .custom(value => value > 0).withMessage('El precio debe ser mayor a cero'),
+    handleInputErrors,
+    createProduct
+);
+
 
 router.put('/', (req, res) => {
     res.json("Desde PUT");
