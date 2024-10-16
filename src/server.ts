1f4a0a2318d76e36425b115b84cf03b5d8e61367
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2024-10-16 18:14:58 -0300

    agregando delete

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import router from "./routes";
 import db from "./config/db";
 import colors from 'colors';
+import cors, { CorsOptions } from 'cors';
+import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec, { swaggerUiOptions } from "./config/swagger";
 
@@ -23,9 +25,24 @@ connectDB();
 
 const server = express();
 
+// Permitir Conexiones
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        if( origin === process.env.FRONTEND_URL){
+            callback(null, true);
+        } else {
+            callback(new Error('Error de CORS'));
+        }
+    }
+}
+
+server.use(cors(corsOptions));
+
 // Leer datos de los JSON
 server.use(express.json());
 
+// Logs en la consola
+server.use(morgan('dev'));
 // Routes
 server.use('/api/products', router);
 
